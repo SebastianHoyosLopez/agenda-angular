8614564adcc9bcc1ec1cf2dad454c02499bc0b51
TatanPC <sebastianhoyos145@gmail.com>
2024-03-05 14:47:28 -0500

    wp: avances con las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { ReservationsComponent } from './reservations/reservations.component';
 
 export const routes: Routes = [
   {
@@ -7,4 +9,14 @@ export const routes: Routes = [
     title: 'Login',
     component: LoginComponent,
   },
+  {
+    path: 'user',
+    title: 'User',
+    component: UserComponent
+  },
+  {
+    path: 'reservations',
+    title: 'Reservations',
+    component: ReservationsComponent
+  }
 ];
